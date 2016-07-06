b900faf1c88ef2618b91dc2e211256ee30b7b745
Carlos Kozuszko <carlos@insignia4u.com>
2016-07-06 17:00:30 -0300

    Quitando restriccion de integridad referencial.

--- db/migrate/20160704193410_create_followings.rb
+++ db/migrate/20160704193410_create_followings.rb
@@ -1,8 +1,8 @@
 class CreateFollowings < ActiveRecord::Migration
   def change
     create_table :followings do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :followee, index: true, foreign_key: true
+      t.references :user, index: true
+      t.references :followee, index: true
 
       t.timestamps null: false
     end
